9431cbcc7cbc133c99049b39334a4770a193b685
rohitmangale2020 <rohit.mangale@oracle.com>
2026-07-29 14:36:59 +0530

    First Commit

---
+++ src/main/java/org/example/moneybags_echo/MoneyBagsEchoApplication.java
@@ -0,0 +1,13 @@
+package org.example.moneybags_echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyBagsEchoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyBagsEchoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/moneybags_echo/MoneyBagsEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moneybags_echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyBagsEchoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
