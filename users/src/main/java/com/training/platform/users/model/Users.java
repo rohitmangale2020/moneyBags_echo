e01f3a3043bb710b56c6294321a819c17b54c897
unknown <rohan.lather@oracle.com>
2026-07-30 11:25:58 +0530

    initialising the setup for User / SMS module

---
+++ users/src/main/java/com/training/platform/users/model/Users.java
@@ -0,0 +1,12 @@
+package com.training.platform.users.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Users {
+
+}
