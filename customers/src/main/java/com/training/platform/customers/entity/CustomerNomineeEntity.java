662819762e64e817f546fbbb3e915af681c35056
sri-dharani-r <SRI.DHARANI.R@ORACLE.COM>
2026-07-30 23:25:23 +0530

    added basic file structure in customer entity

---
+++ customers/src/main/java/com/training/platform/customers/entity/CustomerNomineeEntity.java
@@ -0,0 +1,24 @@
+package com.training.platform.customers.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="customernominee")
+
+public class CustomerNomineeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="nominee_id")
+    private Long nomineeId;
+
+
+
+}
