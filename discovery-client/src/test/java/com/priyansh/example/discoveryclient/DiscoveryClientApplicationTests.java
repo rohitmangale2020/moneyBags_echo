9578bd80414e61f587db8cb97e95f91f1110ebaa
priyansh-pachauri-oracle <priyansh.pachauri@oracle.com>
2026-08-04 11:42:21 +0530

    Add discovery client service

---
+++ discovery-client/src/test/java/com/priyansh/example/discoveryclient/DiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyansh.example.discoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
